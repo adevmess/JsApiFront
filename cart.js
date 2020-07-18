568cbb8923cc06c0a023ffb310f4318e1506d4f9
adevmess <adevmess@gmail.com>
2020-07-19 00:13:25 +0200

    recap commande ok sauf function total

--- cart.js
+++ cart.js
@@ -35,65 +35,107 @@ function toNumber(priceString) {
   return resultToNumber = parseInt(etapeString, 10);
 }
 
-//sous total
-function subTotal() {
-  for (let i = 0; i < cart.length; i++) {
-    let item = cart[i]
-    // console.log(item);
-    let subTotalCart = 0;
-    subTotalCart += item.count * toNumber(item.price);
-    console.log(subTotalCart);
+// //sous total
+// function subTotal() {
+//   for (let i = 0; i < cart.length; i++) {
+//     let item = cart[i]
+//     // console.log(item);
+//     let subTotalCart = 0;
+//     subTotalCart += item.count * toNumber(item.price);
+//     console.log(typeof (subTotalCart));
+//     console.log(subTotalCart);
 
-  }
+//   }
 
-}
-subTotal()
+// }
+// subTotal()
+
+//
 
 //suppression article
 
 //template
-function recapTemplate() {
-  //  let cardTitle = document.createElement("h2");
-  //  cardTitle.id = "card-title";
-  //  cardTitle.textContent = cloneOneCamera.name;
-  //  cardBody.appendChild(cardTitle);
+function recapTemplateProd(cart) {
 
   let productResume = document.querySelector("#productResume");
-
   let tr = document.createElement("tr");
   productResume.appendChild(tr);
 
   let thName = document.createElement("td");
   thName.id = "nameOfProd";
-  thName.innerHTML = "1111111";
+  thName.textContent = cart.name;
   tr.appendChild(thName);
 
   let thCount = document.createElement("td");
-  thCount.innerHTML = "22222222";
+  thCount.textContent = cart.count;
   thCount.id = "countOfProd";
   tr.appendChild(thCount);
 
   let thsubPrice = document.createElement("td");
-  thsubPrice.innerHTML = "33333";
+  thsubPrice.textContent = cart.price;
   thsubPrice.id = "subPrice";
   tr.appendChild(thsubPrice);
 
-  let subToTal = document.createElement("td");
-  subToTal.innerHTML = "subtot";
-  subToTal.id = "subTotal";
-  tr.appendChild(subToTal);
+  let btnDelete = document.createElement("button");
+  btnDelete.className = "btn my-2 btn-danger btn-sm";
+  btnDelete.innerHTML = "X";
+  btnDelete.id = "deleteProduct";
+  tr.appendChild(btnDelete);
 
+
+
+} //on cree notre template en bouclant sur chaque produit de notre panier[i]
+for (let i = 0; i < cart.length; i++) {
+  console.log(cart);
+  recapTemplateProd(cart[i]);
+}
+
+//template Prix Total
+function recapTemplateTotal() {
   let trTotal = document.createElement("tr");
   productResume.appendChild(trTotal);
 
-  let tdTotalCartPrx = document.createElement("td");
+  let tdTotalCartPrx = document.createElement("th");
   tdTotalCartPrx.id = "totalCartPrice";
-  tdTotalCartPrx.innerHTML = "prix Total : ";
+  tdTotalCartPrx.textContent = "prix Total : ";
   trTotal.appendChild(tdTotalCartPrx);
-
-  // for (let i = 0; i < cart.length; i++) {
-  //   console.log(cart);
-  //   (cart[i]);
-  // }
 }
-recapTemplate() //ajouter cart ici
+recapTemplateTotal()
+//ajouter cart ici
+
+
+function saveCart() {
+  localStorage.setItem("shoppingCart", JSON.stringify(cart));
+}
+
+let deleteOneP = document.querySelectorAll("#deleteProduct");
+for (i = 0; i < deleteOneP.length; i++) {
+  deleteOneP[i].addEventListener("click", function (e) {
+    // let reponse = window.confirm("vouler vous supprimer ce produit ?");
+    // if (!reponse) {
+    // event.preventDefault()
+    // } else {
+    //   //removeItemFromCart(id ou name)    //removes one item decremente la qte et delete si 0
+    function removeItemFromCart(name) {
+      for (i in cart) {
+        if (cart[i].id === name) {
+          cart.splice(i, 1);
+          document.location.reload(true);
+          break
+        }
+      }
+      saveCart()
+    }
+
+    removeItemFromCart();
+
+
+
+
+
+
+    // }
+
+  })
+
+}
